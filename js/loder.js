7b2fa63615dabd77a0b2216d597466c290d6a442
Harshit sahu <157795958+Harshit51290@users.noreply.github.com>
2024-03-20 21:27:57 +0530

    Add files via upload

---
+++ js/loder.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide the preloader when the DOM content is fully loaded
+    var preloader = document.querySelector('.wrapper');
+    preloader.style.display = 'none';
+  });
+  
